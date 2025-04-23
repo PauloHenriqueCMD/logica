ad0e1394049d026be82654916c1ed7b077a09995
Geração Tech <geracaotech@digitalcollege.internal>
2025-04-23 16:26:33 -0300

    eu fiz arquivos js e  pratiquei switch

---
+++ cinema.js
@@ -0,0 +1,22 @@
+const readline = require("readline-sync");
+
+let idade = parseInt(readline.question("Qual sua idade"));
+let carteira = readline.question("Você é estudante ou idoso? (s/n)"); 
+if(idade>= 16) {
+    console.log("Entrada permitida.");
+
+if (carteira.toLowerCase() === "s") {
+    console.log("Você tem direito a meia-entrada");
+}
+else {
+    console.log("Você paga o valor integral.");
+}
+}else { 
+    console.log("Entrada não permitida para menores de 16 anos");
+}
+
+
+
+
+
+
