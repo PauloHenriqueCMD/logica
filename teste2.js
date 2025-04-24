90f328b82118fb687372687449e0efc87321fc02
Geração Tech <geracaotech@digitalcollege.internal>
2025-04-24 17:08:17 -0300

    criei primos ao quadrado

---
+++ teste2.js
@@ -0,0 +1,14 @@
+const readline = require("readline-sync");
+const numeros =[];
+let resultado=[]
+    for(let i=1;i<=3;i++){
+        let numero=Number(readline.question('informe o numero' + i +':'));
+        numeros.push(numero)
+    }
+    for(let i=0;i<numeros.length;i++){
+       
+        let numero=numeros[i]*numeros[i]
+        resultado.push(numero)
+    }
+    console.log(resultado)
+    
